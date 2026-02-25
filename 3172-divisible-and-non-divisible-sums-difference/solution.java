e630e17982d9e34429825b6f15552da9354244f5
frost23z <80541095+frost23z@users.noreply.github.com>
2026-02-25 00:11:33 +0000

    Sync LeetCode submissions Runtime - 0 ms (100.00%), Memory - 42.7 MB (27.84%)

---
+++ 3172-divisible-and-non-divisible-sums-difference/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+
+    public int differenceOfSums(int n, int m) {
+        int ans = (n * (n + 1)) / 2;
+
+        if (m == 1) {
+            return -ans;
+        }
+
+        if (m <= n) {
+            int nn = n / m;
+            ans -= nn * (2 * m + (nn - 1) * m);
+        }
+
+        return ans;
+    }
+}
+
